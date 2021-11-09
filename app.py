e98038fb4bc4ba0f8a6a9bebd981ce05bde808b5
Vishal <s.i.n.g.l.a.v.i.s.h.a.l.s.v@gmail.com>
2021-11-10 01:41:22 +0530

    Code to show movies list

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pickle
+
+movies_list = pickle.load(open('movies.pkl','rb'))
+movies_list =movies_list['title'].values
 
 st.title('Movie Recommender System')
 
-option =st.selectbox('How would you like to be contacted?',('Email','Home phone','Mobile phone'))
+option =st.selectbox('How would you like to be contacted?',movies_list)
