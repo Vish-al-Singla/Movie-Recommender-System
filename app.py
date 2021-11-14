91f714011211fd75a14d3a8a89ccaadadd56b68a
Vish-al-Singla <s.i.n.g.l.a.v.i.s.h.a.l.s.v@gmail.com>
2021-11-14 15:07:40 +0530

    Docs: created image for 50 recommendations

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from re import I
 import streamlit as st
 import pickle
 import pandas as pd
 import requests
-
+from itertools import cycle
+from PIL import Image
 
 def fetch_poster(movie_id):
     url ='https://api.themoviedb.org/3/movie/{}?api_key=8d4222eaf0fc55d4781fe3e4f2650478&language=en-US'.format(movie_id)
@@ -18,12 +20,12 @@ def recommend(movie):
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
     recommended_movie_names = []
     recommended_movie_posters = []
-    for i in distances[1:6]:
+    for i in distances[1:51]:
         # fetch the movie poster
         movie_id = movies.iloc[i[0]].movie_id
         recommended_movie_posters.append(fetch_poster(movie_id))
         recommended_movie_names.append(movies.iloc[i[0]].title)
-
+    
     return recommended_movie_names,recommended_movie_posters
 
 movies_list = pickle.load(open('movies.pkl','rb'))
@@ -38,20 +40,6 @@ selected_movie =st.selectbox('Search for Movies here:',movies['title'].values)
 
 if st.button('Show Recommendation'):
     recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
-
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+    cols =cycle(st.columns(4))
+    for idx, recommended_movie_poster in enumerate(recommended_movie_posters):
+        next(cols).image(recommended_movie_poster,width=150, caption=recommended_movie_names[idx])
