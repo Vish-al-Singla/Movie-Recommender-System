7f0529f248c55d4177ac69f7dd401dcfb5661b85
Vish-al-Singla <s.i.n.g.l.a.v.i.s.h.a.l.s.v@gmail.com>
2021-11-14 19:22:21 +0530

    Changes setup.sh

--- setup.sh
+++ setup.sh
@@ -7,12 +7,10 @@ enableCORS = false\n\
 headless = true\n\n
 runOnSave = true\n\
 \n\
-[theme]\n\
-primaryColor='#00a8e8'\n\
-backgroundColor='#00171f'\n\
-secondaryBackgroundColor='#003459'\n\
-textColor='#ffffff'\n\
-font='monospace'\n\
-"
-
- > ~/ .streamlit/config.toml
+[theme]
+primaryColor='#00a8e8'
+backgroundColor='#00171f'
+secondaryBackgroundColor='#003459'
+textColor='#ffffff'
+font='monospace'
+" > ~/ .streamlit/config.toml
